62e853599e58b111e596eb0bdf27485f24031a0a
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-05-12 16:03:05 -0500

    trivial examples, playing with the syntax

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ name = "Mathew Ostrander"
 msg = "Hello World!"
 output = name + " says: " + msg
 print(output)
+
+# del output
+
+if bool(output) == False:
+  print("What happened to my message?")
+elif output:
+  print("I already said something")
+else:
+  print("How did you get here?")
