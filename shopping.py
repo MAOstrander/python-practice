76c2a7a7e98c7bddbe2194d65fb7bdb0df43e7f6
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-05-19 14:39:03 -0500

    streamlined

--- shopping.py
+++ shopping.py
@@ -8,16 +8,23 @@ def show_me(currentlist):
     print(thing)
 
 def instructions():
+  print("")
   print("Type in the items you need from the store, hitting the 'Return' key after each.")
   print("Enter 'SHOW' to see your current list.")
-  print("When you are done with your list enter 'DONE'")
   print("Enter 'HELP' to view these instructions again")
+  print("Enter 'DONE' to finish the list and display")
   print("")
+
+def add_item(thing):
+  shoplist.append(thing)
+  print("Added {} to list, you have {} things to get.".format(thing, len(shoplist)))
 
 
 instructions()
+print("What do you need from the store?")
+
 while True:
-  item = raw_input("What do you need from the store? ")
+  item = raw_input("=> ")
 
   if (item == "DONE"):
     break
@@ -26,7 +33,7 @@ while True:
   elif (item == "HELP"):
     instructions()
   else:
-    shoplist.append(item)
+    add_item(item)
 
 #After we're out of the list show the list
 show_me(shoplist)
