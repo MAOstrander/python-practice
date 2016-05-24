fbd93d07f2af5143f50bbd7392663fe463c2a225
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-05-23 20:49:11 -0500

    Only get five guesses now

--- number_game.py
+++ number_game.py
@@ -1,29 +1,35 @@
 import random
 
-# Generate a number between 1-10
-answer = random.randint(1, 10)
-print(answer)
+def game():
+  # Generate a number between 1-10
+  answer = random.randint(1, 10)
+  guesses = []
 
-while True:
+  while len(guesses) < 5:
 
-  # Let the player guess a number
-  guess = raw_input("Guess a number between 1 and 10: ")
+    # Let the player guess a number
+    guess = raw_input("Guess a number between 1 and 10: ")
 
-  try:
-    guess = int(guess)
-    # Compare the guess to the random number
-    if guess < 1 or guess > 10:
-      print("Hey, something between 1 and 10 please!")
+    try:
+      guess = int(guess)
+      # Compare the guess to the random number
+      if guess < 1 or guess > 10:
+        print("Hey, something between 1 and 10 please!")
 
-    elif guess==answer:
-      break
-    elif guess > answer:
-    # Let the player the result of the comparison
-      print("Sorry, your guess was too high. Try again.")
-    elif guess < answer:
-      print("Sorry, your guess was too low. Try again.")
+      elif guess==answer:
+        print("You got it!!!")
+        break
+      elif guess > answer:
+      # Let the player the result of the comparison
+        print("Sorry, your guess was too high. Try again.")
+      elif guess < answer:
+        print("Sorry, your guess was too low. Try again.")
+      guesses.append(guess)
+    except ValueError:
+      print("That's not a number!")
+  else:
+    print("You lost!")
 
-  except ValueError:
-    print("That's not a number!")
 
-print("You got it!!!")
+
+game()
