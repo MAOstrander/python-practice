ba80aa3215581fadc1ac5f0e46f673fb4c643da1
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-05-25 13:45:34 -0500

    playing with formats

--- letter_game.py
+++ letter_game.py
@@ -1,5 +1,25 @@
 from __future__ import print_function
 import random
+import os
+
+def clear():
+  if os.name == 'nt':
+    os.system('cls')
+  else:
+    os.system('clear')
+
+def display_game(correct, incorrect, answer):
+  clear()
+  print('')
+  print('')
+  print('')
+  for letter in answer:
+    if letter in correct:
+      print(letter, end='')
+    else:
+      print('_', end='')
+  print('')
+  print('Incorrect Guesses: {}/5'.format(len(incorrect)))
 
 def game():
   wordbank = ["bulbasaur", "ivysaur", "venasaur", "squirtle", "wortortle", "blastoise", "charmander", "chareleon", "charizard"]
@@ -8,40 +28,34 @@ def game():
   correct = []
   incorrect = []
 
-  while len(incorrect) < 5 and len(correct) != len(set(answer)):
+  lets_play = raw_input("Press ENTER to play or Q to quit ")
+  if lets_play.lower() != "q":
+    while len(incorrect) < 5 and len(correct) != len(set(answer)):
 
-    for letter in answer:
-      if letter in correct:
-        print(letter, end='')
+      display_game(correct, incorrect, answer)
+
+      guess = raw_input("Try to guess a letter in the mystery word. ").lower()
+
+      if (len(guess) != 1):
+        print("Please only guess a single letter")
+        continue
+      elif guess in correct or guess in incorrect:
+        print("You've already guessed that!")
+        continue
+      elif not guess.isalpha():
+        print("This is a letter game, please only guess letters.")
+        continue
+
+      if guess in answer:
+        correct.append(guess)
+        if len(correct) == len(set(answer)):
+          print("You win! The word was {}!".format(answer))
+          break
       else:
-        print('_', end='')
-    print('')
-    print('Incorrect Guesses: {}/5'.format(len(incorrect)))
-    print('')
+        incorrect.append(guess)
 
-    guess = raw_input("Try to guess a letter in the mystery word. ").lower()
-
-    if (len(guess) != 1):
-      print("Please only guess a single letter")
-      continue
-    elif guess in correct or guess in incorrect:
-      print("You've already guessed that!")
-      continue
-    elif not guess.isalpha():
-      print("This is a letter game, please only guess letters.")
-      continue
-
-    if guess in answer:
-      correct.append(guess)
-      if len(correct) == len(set(answer)):
-        print("You win! The word was {}!".format(answer))
-        break
     else:
-      incorrect.append(guess)
-
-    print(correct)
-  else:
-    print("You lost. :( The word was {}!".format(answer))
+      print("You lost. :( The word was {}!".format(answer))
 
 
 
