67c6923b94e668cf0eb282164034e0d99bbbe0e1
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-05-27 21:39:35 -0500

    attempting to modularize, but battery about to die

--- letter_game.py
+++ letter_game.py
@@ -8,8 +8,24 @@ def clear():
   else:
     os.system('clear')
 
-def display_game(correct, incorrect, answer):
+def validate(correct, incorrect, guess):
+  if guess == '':
+    print("Try to guess the word!")
+  elif (len(guess) > 1):
+    print("Please only guess a single letter")
+    return False
+  elif guess in correct or guess in incorrect:
+    print("You've already guessed that!")
+    return False
+  elif not guess.isalpha():
+    print("This is a letter game, please only guess letters.")
+    return False
+  return True
+
+def display_game(correct, incorrect, answer, guess):
   clear()
+  if validate(correct, incorrect, guess):
+    return False
   print('')
   print('')
   print('')
@@ -19,10 +35,11 @@ def display_game(correct, incorrect, answer):
     else:
       print('_', end='')
   print('')
+  print("Bad letters: {}".format(incorrect))
   print('Incorrect Guesses: {}/5'.format(len(incorrect)))
 
 def game():
-  wordbank = ["bulbasaur", "ivysaur", "venasaur", "squirtle", "wortortle", "blastoise", "charmander", "chareleon", "charizard"]
+  wordbank = ["bulbasaur", "ivysaur", "venasaur", "squirtle", "wortortle", "blastoise", "charmander", "charmeleon", "charizard"]
 
   answer = random.choice(wordbank)
   correct = []
@@ -32,19 +49,11 @@ def game():
   if lets_play.lower() != "q":
     while len(incorrect) < 5 and len(correct) != len(set(answer)):
 
-      display_game(correct, incorrect, answer)
+      display_game(correct, incorrect, answer, "")
 
       guess = raw_input("Try to guess a letter in the mystery word. ").lower()
-
-      if (len(guess) != 1):
-        print("Please only guess a single letter")
-        continue
-      elif guess in correct or guess in incorrect:
-        print("You've already guessed that!")
-        continue
-      elif not guess.isalpha():
-        print("This is a letter game, please only guess letters.")
-        continue
+      if display_game(correct, incorrect, answer, guess):
+        break
 
       if guess in answer:
         correct.append(guess)
