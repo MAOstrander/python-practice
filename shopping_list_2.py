5aa2e6b6ca0b81a84161100df899da9accab7dfe
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-07-20 14:00:13 -0500

    rename file

--- shopping_list_2.py
+++ shopping_list_2.py
@@ -3,12 +3,12 @@ from __future__ import print_function
 shopping_list = []
 
 def show_help():
-  print("\nSeperate eah item with a comma.")
-  print("Type DONE to qui, SHOW to see the current list, and HELP to get this message.")
+  print("\nSeperate each item with a comma.")
+  print("Type DONE to quit, SHOW to see the current list, and HELP to get this message.")
 
 def show_list():
   count = 1
-  for item in show_list:
+  for item in shopping_list:
     print("{}: {}".format(count, item))
     count += 1
 
@@ -16,7 +16,7 @@ print("Give me a list fo things you want to shop for.")
 show_help()
 
 while True:
-  new_stuff = input("> ")
+  new_stuff = raw_input("> ")
 
   if new_stuff == "DONE":
     print("\nHere's your list:")
@@ -30,4 +30,4 @@ while True:
     continue
   else:
     new_list = new_stuff.split(",")
-    index = input("Do you want to reorder your list? Press enter to put item at the end of list. Type a number and hit enter to put item at that spot.\n You have {} item(s) in your list.".format(len(shopping_list)))
+    index = raw_input("Do you want to reorder your list? Press enter to put item at the end of list. Type a number and hit enter to put item at that spot.\n You have {} item(s) in your list.".format(len(shopping_list)))
