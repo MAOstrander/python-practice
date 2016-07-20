b6e9701e2260324e393ef72941ceca94a963949e
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-07-20 14:34:20 -0500

    can remove items from list

--- shopping_list_2.py
+++ shopping_list_2.py
@@ -4,7 +4,7 @@ shopping_list = []
 
 def show_help():
   print("\nSeperate each item with a comma.")
-  print("Type DONE to quit, SHOW to see the current list, and HELP to get this message.")
+  print("Type EXIT to quit, SHOW to see the current list, DONE to remove an item from the list, and HELP to get this message.")
 
 def show_list():
   count = 1
@@ -12,13 +12,16 @@ def show_list():
     print("{}: {}".format(count, item))
     count += 1
 
+def done_with_item(gone):
+  del shopping_list[gone]
+
 print("Give me a list fo things you want to shop for.")
 show_help()
 
 while True:
   new_stuff = raw_input("> ")
 
-  if (new_stuff == "DONE"):
+  if (new_stuff == "EXIT"):
     print("\nHere's your list:")
     show_list()
     break
@@ -27,6 +30,12 @@ while True:
     continue
   elif (new_stuff == "SHOW"):
     show_list()
+    continue
+  elif (new_stuff == "DONE"):
+    show_list()
+    del_index = raw_input("Which item do you want to remove? Type the number and hit enter to that item.\n You have {} item(s) in your list.".format(len(shopping_list)))
+    del_index = int(del_index) - 1
+    done_with_item(del_index)
     continue
   else:
     new_list = new_stuff.split(",")
