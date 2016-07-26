edb59d901e85c21b16b47872a599351e21afddf4
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-07-25 21:27:40 -0500

    python slices are much cooler than javascript slice/splice

---
+++ slices.py
@@ -0,0 +1,11 @@
+big_list = list(range(1,50))
+print big_list
+
+everything_even = big_list[1::2]
+print everything_even
+
+words = "Thisisanexamplestringfulloflettersnospaces"
+print words[::-1]
+
+middle_chunk_backwards = big_list[30:10:-1]
+print middle_chunk_backwards
