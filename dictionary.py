f59ad7697347b5082a1da52db7618995b852a7fe
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-07-27 13:32:49 -0500

    modifying values in a dictionary in python

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,28 @@
 my_dict = {'apples': 1, 'bananas': 2, 'coconuts': 3}
 my_list = ['apples', 'coconuts', 'grapes', 'strawberries']
 
-def members(arg1, arg2):
+# Count the number of items that overlap between a dictionary and list
+def members(dictArg, listArg):
     count = 0
-    for thing in arg1:
+    for thing in dictArg:
       print thing
-      print arg1
-      print arg2
-      if (thing in arg2):
+      print dictArg
+      print listArg
+      if (thing in listArg):
         count = count + 1
     return count
 
 answer = members(my_dict, my_list)
 print answer
+
+# give a count of how many times each word appears in a sentence
+def word_count(stringArg):
+    counted_words = {}
+    word_list = stringArg.split()
+    for word in word_list:
+        if (word in counted_words):
+            counted_words[word] = counted_words[word] + 1
+        else:
+            counted_words[word] =  1
+
+    return counted_words
